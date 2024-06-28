49cfaa0cde5ed7c37a56e8f21194bd1b53ed4202
sullaymansamai3@gmail.com <sullaymansamai3@gmail.com>
2024-06-28 09:01:08 +0000

    work on my c# cod

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("what is your last name? ");
+        string last = Console.ReadLine();
+
     }
+    
 }
