ab7db8783a3350f605cf6518520db538359ed415
sullaymansamai3@gmail.com <sullaymansamai3@gmail.com>
2024-07-13 19:16:24 +0000

    making changes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,40 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+        // Square sArea = new Square("red", 5);
+        // // string color = sArea.GetColor();
+        // // double area = sArea.GetArea();
+
+        // Rectangle rArea = new Rectangle("blue", 4, 5);
+        // // string color = rArea.GetColor();
+        // // double area = rArea.GetArea();
+
+        // Circle cArea = new Circle("green", 6);
+        // string color = cArea.GetColor();
+        // double area = cArea.GetArea();
+
+        // Console.WriteLine($"The {color} shape has an area of {area}");
+
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square ("red", 3);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("blue", 4, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("green", 5);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+            
+        }
+
     }
 }
---
+++ prove/Develop05/SaveLoadCSV.cs
@@ -0,0 +1,22 @@
+class SaveLoadCSV
+{
+    public static void SaveToCSV(List<String> _dataToSave, string _filePath)
+    {
+        File.WriteAllLines(_filePath, _dataToSave);
+    }
+
+    public static List<String> LoadFromCSV(string _filePath)
+    {
+        List<string> fromCSV = new List<string>();
+        StreamReader streamReader = new StreamReader(_filePath);
+        
+        while (!streamReader.EndOfStream)
+        {
+            string line = streamReader.ReadLine();
+            fromCSV.Add(line.ToString());
+        }   
+        streamReader.Close();
+
+        return fromCSV;
+    }   
+}
